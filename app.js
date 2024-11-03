10c7ec18add3d7ec810f23de1bccc669978cabff
revanth332 <revathipathilanka347@gmail.com>
2024-11-03 22:10:20 +0530

    added multi tickets

--- app.js
+++ app.js
@@ -12,30 +12,42 @@ const io = socketIo(server, {
     methods: ["GET", "POST"],
   },
 });
-console.log(process.env.FRONTEND_URL)
+console.log(process.env.FRONTEND_URL);
 const port = process.env.PORT || 3000;
 
 var rooms = [];
-var roomUsers = {}
+var roomUsers = {};
 
 // app.use(cors({ origin: ["http://172.17.10.127:5173"] }));
 app.use(cors());
 app.use(express.json());
 
-app.get("/test",(req,res) => {
-  return res.send("test")
-})
+app.get("/test", (req, res) => {
+  return res.send("test");
+});
 
 app.post("/api/room", (req, res) => {
   // console.log(req.body);
-  const { type, roomCode, user } = req.body;
-  const username = user;
+  const { type, roomCode, username } = req.body;
   if (type === "createRoom") {
     if (!rooms.includes(roomCode)) {
       rooms.push(roomCode);
-      roomUsers[roomCode] = [{username,micAllowed:false}];
-      console.log("room created"+roomCode);
-      return res.json({ success: true, room: roomCode,users:[{username,micAllowed:false}] });
+      roomUsers[roomCode] = [
+        {
+          username,
+          micAllowed: false,
+          roomNumber: roomCode,
+          isAdmin: true,
+          ticketCount: 0,
+          isEditing: false,
+        },
+      ];
+      console.log("room created" + roomCode);
+      return res.json({
+        success: true,
+        room: roomCode,
+        users: roomUsers[roomCode],
+      });
     } else {
       console.log([...rooms]);
       return res.json({
@@ -46,12 +58,22 @@ app.post("/api/room", (req, res) => {
     }
   } else {
     if (rooms.includes(roomCode)) {
-      if(!roomUsers[roomCode].includes(username)){
-        roomUsers[roomCode].push({username,micAllowed:false});
+      if (!roomUsers[roomCode].find(user => user.username === username)) {
+        roomUsers[roomCode].push({
+          username,
+          micAllowed: false,
+          roomNumber: roomCode,
+          isAdmin: false,
+          ticketCount: 0,
+          isEditing: false,
+        });
         // console.log("user entered ")
-        return res.json({ success: true, room: roomCode,users:roomUsers[roomCode] });
-      }
-      else{
+        return res.json({
+          success: true,
+          room: roomCode,
+          users: roomUsers[roomCode],
+        });
+      } else {
         // console.log("user exists ")
         return res.json({
           success: false,
@@ -73,54 +95,73 @@ app.post("/api/room", (req, res) => {
 io.on("connection", (socket) => {
   // console.log("A user connected");
 
-  socket.on("housie", (newHousie,currentUser) => {
-    io.emit("housie", newHousie,currentUser);
+  socket.on("housie", (newHousie, currentUser) => {
+    console.log(newHousie.participants)
+    io.emit("housie", newHousie, currentUser);
   });
 
   socket.on("disconnect", () => {
     // console.log("User disconnected");
   });
 
-  socket.on('audioStream', (audioData,currentUser) => {
-    if(currentUser.info.micAllowed) socket.broadcast.emit('audioStream', audioData,currentUser);
+  socket.on("audioStream", (audioData, currentUser) => {
+    if (currentUser.info.micAllowed)
+      socket.broadcast.emit("audioStream", audioData, currentUser);
   });
 
-  socket.on("entered",(newwHousie) => {
-    socket.broadcast.emit('entered', newwHousie);
+  socket.on("entered", (newHousie, newUser) => {
+    const updatedHousie = {
+      ...newHousie,
+      participants: roomUsers[newHousie.roomNumber],
+    };
+    // console.log(updatedHousie.participants);
+    socket.broadcast.emit("entered", updatedHousie, newUser);
+  });
+
+  socket.on("micAllow",(newHousie) => {
+    socket.broadcast.emit("micAllow",newHousie)
   })
 
-  socket.on("jaldi5Complete",(currentUser) => {
-    console.log(user,"jaldi5")
-    socket.broadcast.emit("jaldi5",currentUser);
-  })
-  
-  socket.on("row1Complete",(currentUser) => {
-    socket.broadcast.emit("row1Complete",currentUser);
+  socket.on("ticketUpdate",(newTicketCount,selectedUser,newHousie,generatedTickets) => {
+    socket.broadcast.emit("ticketUpdate",newTicketCount,selectedUser,newHousie,generatedTickets)
   })
 
-  socket.on("row2Complete",(currentUser) => {
-    socket.broadcast.emit("row2Complete",currentUser);
-  })
+  socket.on("jaldi5Complete", (newHousie,currentUser) => {
+    socket.broadcast.emit("jaldi5", newHousie,currentUser);
+  });
 
-  socket.on("row3Complete",(currentUser) => {
-    socket.broadcast.emit("row3Complete",currentUser);
-  })
+  socket.on("row1Complete", (newHousie,currentUser) => {
+    socket.broadcast.emit("row1Complete", newHousie,currentUser);
+  });
 
-  socket.on("win",(currentUser) => {
-    socket.broadcast.emit("win",currentUser);
-  })
+  socket.on("row2Complete", (newHousie,currentUser) => {
+    socket.broadcast.emit("row2Complete", newHousie,currentUser);
+  });
 
-  socket.on("exit",( newHousie,currentUser) => {
-    if(currentUser.info.isAdmin){
-      rooms = rooms.filter(roomNo => roomNo != newHousie.roomNumber);
+  socket.on("row3Complete", (newHousie,currentUser) => {
+    socket.broadcast.emit("row3Complete", newHousie,currentUser);
+  });
+
+  socket.on("win", (newHousie,currentUser) => {
+    socket.broadcast.emit("win", newHousie,currentUser);
+  });
+
+  socket.on("exit", (newHousie, currentUser) => {
+    console.log(newHousie.participants);
+    if (currentUser.info.isAdmin) {
+      rooms = rooms.filter((roomNo) => roomNo != newHousie.roomNumber);
       delete roomUsers[newHousie.roomNumber];
+    } else {
+      roomUsers = {
+        ...roomUsers,
+        [newHousie.roomNumber]: roomUsers[newHousie.roomNumber]?.filter(
+          (item) => currentUser.info.username != item.username
+        ),
+      };
     }
-    else{
-      roomUsers = {...roomUsers,[newHousie.roomNumber]:roomUsers[newHousie.roomNumber]?.filter(item => user != item.username)}; // modifying.....
-    }
-    socket.broadcast.emit("exit",newHousie,currentUser);
-  })
-
+    // console.log(roomUsers[newHousie.roomNumber]?.length);
+    socket.broadcast.emit("exit", newHousie, currentUser);
+  });
 });
 
 server.listen(port, () => {
